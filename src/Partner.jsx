b1d8f8001b512ca63665ab5bc04c264c783ef1ea
NurTheDev <nur756.islam@gmail.com>
2025-01-29 23:49:11 +0600

    @NurTheDev added partner Part

--- src/Partner.jsx
+++ src/Partner.jsx
@@ -1,23 +1,52 @@
 import Title from "./component/common/Title";
+import brave from "./assets/partner/brave.png";
+import firefox from "./assets/partner/firefox.png";
+import opera from "./assets/partner/opera.png";
+import google from "./assets/partner/google.png";
+import safari from "./assets/partner/safari.png";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 const Partner = () => {
   const partner = [
-    { id: 1, img: "./assets/partner/brave.png" },
-    { id: 2, img: "./assets/partner/firefox.png" },
-    { id: 3, img: "./assets/partner/opera.png" },
-    { id: 4, img: "./assets/partner/google.png" },
-    { id: 5, img: "./assets/partner/safari.png" },
-    { id: 6, img: "./assets/partner/window.png" },
+    { id: 1, img: brave },
+    { id: 2, img: firefox },
+    { id: 3, img: opera },
+    { id: 4, img: google },
+    { id: 5, img: safari },
+    // { id: 6, img: window },
   ];
+  console.log(partner);
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+  };
   return (
-    <div className="text-center mt-24">
+    <div className="text-center my-24 container">
       <Title firstText={"Our"} secondText={"Partner"} />
-      <div>
+      <Slider {...settings} className="w-4/5 mx-auto mt-16">
         {partner.map((data) => (
-          <div key={data.id}>
-            <img src={data.img} alt={`Partner ${data.id}`} />
+          <div key={data.id} className="px-8">
+            {" "}
+            {/* Add padding for gap */}
+            <div className="bg-[#F6F6F6] p-6 rounded-xl flex items-center justify-center shadow-md">
+              <img
+                className="w-[120px] h-[120px] object-contain imgGrayscale"
+                src={data.img}
+                alt={`Partner ${data.id}`}
+              />
+            </div>
           </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 };
